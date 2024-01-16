2afcc67dadb40ba0ae23ccd6bc92c426aab65210
Varun Gumma <t-gummavarun@microsoft.com>
2024-01-16 04:25:13 +0000

    bug fixes for LRL tags

--- IndicTransTokenizer/tokenizer.py
+++ IndicTransTokenizer/tokenizer.py
@@ -26,16 +26,23 @@ class IndicTransTokenizer:
 
         self.supported_langs = [
             "asm_Beng",
+            "awa_Deva",
             "ben_Beng",
+            "bho_Deva",
             "brx_Deva",
             "doi_Deva",
             "eng_Latn",
             "gom_Deva",
+            "gon_Deva",
             "guj_Gujr",
             "hin_Deva",
+            "hne_Deva",
             "kan_Knda",
             "kas_Arab",
             "kas_Deva",
+            "kha_Latn",
+            "lus_Latn",
+            "mag_Deva",
             "mai_Deva",
             "mal_Mlym",
             "mar_Deva",
@@ -51,6 +58,7 @@ class IndicTransTokenizer:
             "tam_Taml",
             "tel_Telu",
             "urd_Arab",
+            "unr_Deva"
         ]
 
         self.src_vocab_fp = (
